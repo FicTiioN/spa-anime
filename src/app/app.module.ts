c19671220f65dd85fed61504d962860574aa9c09
FicTiioN <fic_leo@outlook.com>
2021-09-21 15:07:26 -0300

    generate pilot components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardAnimeComponent } from './shared/components/card-anime/card-anime.component';
+import { AnimeComponent } from './components/anime/anime.component';
+import { AnimeDetailComponent } from './components/anime-detail/anime-detail.component';
+import { EpisodioComponent } from './components/episodio/episodio.component';
+import { EpisodioListComponent } from './components/episodio-list/episodio-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardAnimeComponent,
+    AnimeComponent,
+    AnimeDetailComponent,
+    EpisodioComponent,
+    EpisodioListComponent,
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/card-anime/card-anime.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-anime',
+  templateUrl: './card-anime.component.html',
+  styleUrls: ['./card-anime.component.scss']
+})
+export class CardAnimeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
